b5cb24cbe0da4f10b9b49a3c9e9a1bda5f110499
caymayhigaye-creator <caymayhigaye@gmail.com>
2026-04-14 12:53:21 +0300

    c

---
+++ __express.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+const PORT = 3000 || 3000;
+app.listen(PORT, () => {
+    console.log('3000 Port is working rn');
+});
+
+app.post('/$models', async (request, response) => {
+    console.log(response.body);
+
+    response.status(200).send('info claimed');
+}) 
+
+export {app};
